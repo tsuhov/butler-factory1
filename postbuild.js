c202f6b0aadcb73282d8e1527f3dadb7c130f380
melvinmilillo <melvinmilillo@gmail.com>
2025-07-16 23:34:23 -0700

    Update postbuild.js
    не было домена

--- postbuild.js
+++ postbuild.js
@@ -1,40 +1,43 @@
-import fs from 'fs';
+// Файл: postbuild.js
+import fs from 'fs/promises';
 import path from 'path';
 
+// ВАЖНО: Замените этот адрес на ваш реальный адрес Netlify
+const siteUrl = 'https://butlerspb-blog.netlify.app'; 
 const publicDir = './dist';
-const sitemapPath = './dist/sitemap.xml';
-const siteUrl = 'https://ВАШ-БУДУЩИЙ-АДРЕС.netlify.app'; // Мы заменим это позже
 
-function getHtmlFiles(dir) {
-    let files = [];
-    const items = fs.readdirSync(dir, { withFileTypes: true });
+async function generateSitemap() {
+    console.log('--- Запуск генератора Sitemap.xml ---');
+    try {
+        const files = await fs.readdir(publicDir, { recursive: true });
+        const htmlFiles = files.filter(file => file.endsWith('.html'));
 
-    for (const item of items) {
-        const fullPath = path.join(dir, item.name);
-        if (item.isDirectory()) {
-            files = [...files, ...getHtmlFiles(fullPath)];
-        } else if (item.name.endsWith('.html')) {
-            files.push(fullPath);
-        }
-    }
-    return files;
-}
-
-const allFiles = getHtmlFiles(publicDir);
-const urls = allFiles.map(file => {
-    const urlPath = file.replace(publicDir, '').replace(/\\/g, '/').replace('index.html', '');
-    return `
+        const urls = htmlFiles.map(file => {
+            let relativePath = path.join('/', file).replace(/\\/g, '/'); // Нормализуем путь
+            if (relativePath.endsWith('index.html')) {
+                relativePath = relativePath.slice(0, -10); // Убираем 'index.html', оставляя '/'
+            } else if (relativePath.endsWith('.html')) {
+                relativePath = relativePath.slice(0, -5); // Убираем '.html'
+            }
+            return `
     <url>
-        <loc>${siteUrl}${urlPath}</loc>
+        <loc>${siteUrl}${relativePath}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <priority>0.8</priority>
     </url>`;
-});
+        });
 
-const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+        const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urls.join('')}
 </urlset>`;
 
-fs.writeFileSync(sitemapPath, sitemapContent);
-console.log(`[✔] Sitemap.xml успешно сгенерирован! Найдено ${urls.length} страниц.`);
+        await fs.writeFile(path.join(publicDir, 'sitemap.xml'), sitemapContent);
+        console.log(`[✔] Sitemap.xml успешно сгенерирован! Найдено ${urls.length} страниц.`);
+
+    } catch (error) {
+        console.error('[!] Ошибка при генерации Sitemap.xml:', error);
+    }
+}
+
+generateSitemap();
