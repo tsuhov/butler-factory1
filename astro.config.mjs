7e8e8792fbde493b3117b501370e29cbd3d4f8e0
melvinmilillo <melvinmilillo@gmail.com>
2025-07-21 12:24:03 -0700

    Update astro.config.mjs
    сайтмап

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://butlerspb-blog.netlify.app' 
+  // Указываем здесь правильный, финальный URL вашего сайта
+  site: 'https://butlerspb-blog.netlify.app',
+  
+  // Включаем официальную интеграцию для автоматической генерации sitemap.xml
+  integrations: [
+    sitemap()
+  ]
 });
