dd9051a23736053b7dd2f48489b0bb5d89a0e6f5
melvinmilillo <melvinmilillo@gmail.com>
2025-07-21 15:45:42 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://butlerspb-blog.netlify.app',
-  // Мы полностью убрали блок integrations
 });
