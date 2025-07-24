1783372dd49603a78d3a5450c0e15a7cda3267b0
melvinmilillo <melvinmilillo@gmail.com>
2025-07-24 07:20:10 -0700

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,19 @@
-// Файл: src/pages/rss.xml.js (Версия 2.1 - Исправленная)
+// Файл: src/pages/rss.xml.js (Версия 2.2 - Ограничение по количеству)
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { marked } from 'marked';
 import sanitizeHtml from 'sanitize-html';
 
-// ИСПРАВЛЕНИЕ: Мы не импортируем consts.ts, а задаем константы прямо здесь
-const SITE_TITLE = 'ButlerSPB';
-const SITE_DESCRIPTION = 'ButlerSPB';
+const POST_LIMIT = 30; // ИСПРАВЛЕНИЕ: Ограничиваем количество статей в фиде
 
 export async function GET(context) {
 	const posts = await getCollection('posts');
 	
+    // ИСПРАВЛЕНИЕ: Сортируем посты по дате и берем только последние 30
+    const sortedPosts = posts.sort((a, b) => new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf());
+    const limitedPosts = sortedPosts.slice(0, POST_LIMIT);
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
@@ -24,7 +27,7 @@ export async function GET(context) {
             'media': 'http://search.yahoo.com/mrss/',
             'content': 'http://purl.org/rss/1.0/modules/content/'
         },
-		items: posts.map((post) => {
+		items: limitedPosts.map((post) => { // Используем ограниченный список
             const rawHtml = marked.parse(post.body);
             const cleanHtml = sanitizeHtml(rawHtml, {
                 allowedTags: [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'ul', 'li', 'a', 'br', 'img', 'figure', 'figcaption', 'b', 'strong', 'i', 'em', 's', 'strike', 'del', 'blockquote' ],
