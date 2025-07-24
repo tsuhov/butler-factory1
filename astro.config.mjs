1baacb194c6789b65ab327709a53e03d0095233c
melvinmilillo <melvinmilillo@gmail.com>
2025-07-24 07:07:00 -0700

    Update astro.config.mjs
    РСС

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://butlerspb-blog.netlify.app',
+  integrations: [sitemap()], // <-- Эта строка "включает" генератор карты сайта
+  vite: {
+    ssr: {
+      external: ["sanitize-html"], // <-- Эта строка "чинит" ошибку со сборкой RSS
+    },
+  },
 });
