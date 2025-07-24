9267946a1114b16cf6c9d30ca3fa532b86a67da9
melvinmilillo <melvinmilillo@gmail.com>
2025-07-24 06:57:39 -0700

    Create rss.xml.js
    РСС лента

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,43 @@
+// Файл: src/pages/rss.xml.js (Версия для Яндекс.Новостей)
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { marked } from 'marked';
+import sanitizeHtml from 'sanitize-html';
+
+export async function GET(context) {
+	const posts = await getCollection('posts');
+	
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		customData: `
+            <yandex:logo>${context.site}favicon.ico</yandex:logo>
+            <yandex:logo type="square">${context.site}favicon.ico</yandex:logo>
+        `,
+        xmlns: {
+            'yandex': 'http://news.yandex.ru',
+            'media': 'http://search.yahoo.com/mrss/',
+            'content': 'http://purl.org/rss/1.0/modules/content/'
+        },
+		items: posts.map((post) => {
+            const rawHtml = marked.parse(post.body);
+            const cleanHtml = sanitizeHtml(rawHtml, {
+                allowedTags: [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'ul', 'li', 'a', 'br', 'img', 'figure', 'figcaption', 'b', 'strong', 'i', 'em', 's', 'strike', 'del', 'blockquote' ],
+                allowedAttributes: {
+                    'a': [ 'href' ],
+                    'img': [ 'src', 'alt' ]
+                }
+            });
+
+            return {
+                link: `/blog/${post.slug}/`,
+                title: post.data.title,
+                pubDate: post.data.pubDate,
+                description: post.data.description,
+                customData: `<content:encoded><![CDATA[${cleanHtml}]]></content:encoded>`,
+            }
+        }),
+	});
+}
